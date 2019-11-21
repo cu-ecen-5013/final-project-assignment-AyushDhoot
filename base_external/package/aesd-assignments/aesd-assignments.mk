b9d08f6500761febc26c84e61008da54d9f9cc3d
shubjais <shja7942@colorado.edu>
2019-11-20 20:35:13 -0700

    Added client-socket package to buildroot

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -13,16 +13,12 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/socket_server all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client-socket all
 endef
 
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-<<<<<<< HEAD
-	$(INSTALL) -m 0755 $(@D)/socket_server/server-socket $(TARGET_DIR)/bin
-=======
 	$(INSTALL) -m 0755 $(@D)/client-socket/client-socket $(TARGET_DIR)/bin
->>>>>>> Changed commit hash to include client-socket utility
 endef
 
 
