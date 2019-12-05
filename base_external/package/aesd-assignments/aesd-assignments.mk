c56d93f6259cbe534fb2e814322d5654a9ac8496
shubjais <shja7942@colorado.edu>
2019-12-04 21:04:24 -0700

    Added dropbear and changed commit hash to include temperature sensor executable

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,18 +7,20 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 
-AESD_ASSIGNMENTS_VERSION = 54cc283a3cd9a762df1c949399d6e42104ae1b16
+AESD_ASSIGNMENTS_VERSION = 99f1e9fa021c71bc359f15295c28168ea1a0bc23
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-assignment-shubjais.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client-socket all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/tmp102 all
 endef
 
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/client-socket/client-socket $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/tmp102/tmp102 $(TARGET_DIR)/bin
 endef
 
 
