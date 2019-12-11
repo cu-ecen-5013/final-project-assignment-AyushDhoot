d1640f5e15a8413a86b5d332fe27d3765a8470fe
shubjais <shja7942@colorado.edu>
2019-12-10 19:04:17 -0700

    Changed from Uclibc to glibc

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,20 +7,18 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 
-AESD_ASSIGNMENTS_VERSION = 0d2774f59d3b1b50c83b45b8a235e1081baa3538
+AESD_ASSIGNMENTS_VERSION = c486e4c4448b37829a71ba56d84c6fb6073ddcf6
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-assignment-shubjais.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client-socket all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/tmp102 all
 endef
 
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/client-socket/client-socket $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/tmp102/tmp102 $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/client-socket/client_socket $(TARGET_DIR)/bin
 endef
 
 
